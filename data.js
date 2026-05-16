c98b6bbf908fa546979bb3a8df472d6c18f7d5b6
Nghĩalq TV <nguyentrungnghia20012004@gmail.com>
2026-05-17 03:02:26 +0700

    Add pages object with links for multiple users

---
+++ data.js
@@ -0,0 +1,51 @@
+const pages = {
+
+  aoi:{
+
+    sub:
+    "https://youtube.com/@aoi",
+
+    like:
+    "https://youtu.be/aoi123",
+
+    tele:
+    "https://t.me/aoi",
+
+    unlock:
+    "https://www.mediafire.com/file/aoi"
+
+  },
+
+  nakroth:{
+
+    sub:
+    "https://youtube.com/@nakroth",
+
+    like:
+    "https://youtu.be/nak456",
+
+    tele:
+    "https://t.me/nakroth",
+
+    unlock:
+    "https://www.mediafire.com/file/nak"
+
+  },
+
+  allain:{
+
+    sub:
+    "https://youtube.com/@allain",
+
+    like:
+    "https://youtu.be/allain789",
+
+    tele:
+    "https://t.me/allain",
+
+    unlock:
+    "https://www.mediafire.com/file/allain"
+
+  }
+
+};
