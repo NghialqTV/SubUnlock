121ddf03717ec28e2d73d05c1d9d752ec9fd4484
Nghĩalq TV <nguyentrungnghia20012004@gmail.com>
2026-05-17 03:25:58 +0700

    Update data.js

--- data.js
+++ data.js
@@ -3,15 +3,17 @@ const pages = {
   Menu:{
 
     sub:
-    "https://youtube.com/@nghiaxtv?si=4aLD_3t9rD_Hmo8m",
+    "https://youtube.com/@nghiaxtv",
 
     like:
-    "https://youtu.be/MZLKU7WZbRA?si=w87VAZ1-tCLsC5_4",
+    "https://youtu.be/MZLKU7WZbRA",
 
     tele:
     "https://t.me/+gI22PHmUi5xhMDA9",
 
     unlock:
-    "https://www.mediafire.com/file/2ylxdv8p8vn7i3d/Menu_Mod_Skin_%255B_64_Bit_%255D.apk/file"
+    "https://www.mediafire.com/file/2ylxdv8p8v"
 
-  };
+  }
+
+};
