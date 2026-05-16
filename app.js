7f3798713ae046b5c96e8955a507841dc0710d7a
Nghĩalq TV <nguyentrungnghia20012004@gmail.com>
2026-05-17 04:09:16 +0700

    Update app.js

--- app.js
+++ app.js
@@ -39,15 +39,9 @@ function updateProgress(){
   if(done3) total += 34;
 
   document.getElementById(
-  'progress'
-).style.width =
-total + "%";
-
-
-document.getElementById(
-  'percent'
-).innerHTML =
-total + "%";
+    'progress'
+  ).style.width =
+  total + "%";
 }
 
 // SUBSCRIBE
