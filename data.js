0a23e16524394a9c3fb7db610f3ad2e7eb000a3c
Nghĩalq TV <nguyentrungnghia20012004@gmail.com>
2026-05-17 03:17:21 +0700

    Rename 'aoi' to 'Menu' and update links

--- data.js
+++ data.js
@@ -1,18 +1,18 @@
 const pages = {
 
-  aoi:{
+  Menu:{
 
     sub:
-    "https://youtube.com/@aoi",
+    "https://youtube.com/@nghiaxtv?si=4aLD_3t9rD_Hmo8m",
 
     like:
-    "https://youtu.be/aoi123",
+    "https://youtu.be/MZLKU7WZbRA?si=w87VAZ1-tCLsC5_4",
 
     tele:
-    "https://t.me/aoi",
+    "https://t.me/+gI22PHmUi5xhMDA9",
 
     unlock:
-    "https://www.mediafire.com/file/aoi"
+    "https://www.mediafire.com/file/2ylxdv8p8vn7i3d/Menu_Mod_Skin_%255B_64_Bit_%255D.apk/file"
 
   },
 
