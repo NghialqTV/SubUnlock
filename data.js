185f78be1f53de8b966d0ee2aad418db7bc92690
Nghĩalq TV <nguyentrungnghia20012004@gmail.com>
2026-05-17 09:08:06 +0700

    Add keymenuios section with various links

--- data.js
+++ data.js
@@ -30,6 +30,22 @@ const pages = {
     unlock:
     "https://ipas.cc/anoq8i"
 
+  },
+  
+  keymenuios: {
+
+    sub:
+    "https://youtube.com/@nghiaxtv",
+
+    like:
+    "https://youtu.be/8e_VkmBs5Yk?si=C_xDgPkr9-7XoVu3",
+
+    tele:
+    "https://t.me/+gI22PHmUi5xhMDA9",
+
+    unlock:
+    "https://funlink.io/Puev89R"
+
   }
 
 };
