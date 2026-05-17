52b8739e97623614d7a72497056df5e518e6fe67
Nghĩalq TV <nguyentrungnghia20012004@gmail.com>
2026-05-17 18:12:24 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,63 +1,32 @@
 const pages = {
 
   hackmenuios: {
-
-    sub:
-    "https://youtube.com/@nghiaxtv",
-
-    like:
-    "https://youtu.be/8e_VkmBs5Yk?si=C_xDgPkr9-7XoVu3",
-
-    tele:
-    "https://t.me/+gI22PHmUi5xhMDA9",
-
-    unlock:
-    "https://ipas.cc/82k69h"
-
+    sub: "https://youtube.com/@nghiaxtv",
+    like: "https://youtu.be/8e_VkmBs5Yk?si=C_xDgPkr9-7XoVu3",
+    tele: "https://t.me/+gI22PHmUi5xhMDA9",
+    unlock: "https://ipas.cc/82k69h"
   },
 
   modmenuios: {
-
-    sub:
-    "https://youtube.com/@nghiaxtv",
-
-    like:
-    "https://youtu.be/8e_VkmBs5Yk?si=C_xDgPkr9-7XoVu3",
-
-    tele:
-    "https://t.me/+gI22PHmUi5xhMDA9",
-
-    unlock:
-    "https://ipas.cc/anoq8i"
-
+    sub: "https://youtube.com/@nghiaxtv",
+    like: "https://youtu.be/8e_VkmBs5Yk?si=C_xDgPkr9-7XoVu3",
+    tele: "https://t.me/+gI22PHmUi5xhMDA9",
+    unlock: "https://ipas.cc/anoq8i"
   },
-  
+
   keymenuios: {
-
-    sub:
-    "https://youtube.com/@nghiaxtv",
-
-    like:
-    "https://youtu.be/8e_VkmBs5Yk?si=C_xDgPkr9-7XoVu3",
-
-    tele:
-    "https://t.me/+gI22PHmUi5xhMDA9",
-
-    unlock:
-    "https://funlink.io/Puev89R"
-
+    sub: "https://youtube.com/@nghiaxtv",
+    like: "https://youtu.be/8e_VkmBs5Yk?si=C_xDgPkr9-7XoVu3",
+    tele: "https://t.me/+gI22PHmUi5xhMDA9",
+    unlock: "https://funlink.io/Puev89R"
   },
+
   pack40skin1705: {
-
-    sub:
-    "https://youtube.com/@nghiaxtv",
-
-    like:
-    "https://youtu.be/hcb2ZsVcQ1E?si=dcW7h64IvZre1C1N",
-
-    tele:
-    "https://t.me/+gI22PHmUi5xhMDA9",
-
+    sub: "https://youtube.com/@nghiaxtv",
+    like: "https://youtu.be/hcb2ZsVcQ1E?si=dcW7h64IvZre1C1N",
+    tele: "https://t.me/+gI22PHmUi5xhMDA9",
     unlock:
-    "https://www.mediafire.com/file/6flzvt1eklfzagd/Pack_40_Skin_By_Ngh%25C4%25A9aLq_TV_%255B_Android_-_IOS_%255D.zip/file"
+      "https://www.mediafire.com/file/6flzvt1eklfzagd/Pack_40_Skin_By_Ngh%25C4%25A9aLq_TV_%255B_Android_-_IOS_%255D.zip/file"
+  }
+
 };
