a4f175c7036fda862253535b059b5a4f10ceff71
Nghĩalq TV <nguyentrungnghia20012004@gmail.com>
2026-05-17 19:35:56 +0700

    Update data.js

--- data.js
+++ data.js
@@ -25,8 +25,7 @@ const pages = {
     sub: "https://youtube.com/@nghiaxtv",
     like: "https://youtu.be/hcb2ZsVcQ1E?si=dcW7h64IvZre1C1N",
     tele: "https://t.me/+gI22PHmUi5xhMDA9",
-    unlock:
-      "https://www.mediafire.com/file/6flzvt1eklfzagd/Pack_40_Skin_By_Ngh%25C4%25A9aLq_TV_%255B_Android_-_IOS_%255D.zip/file"
+    unlock: "https://www.mediafire.com/file/6flzvt1eklfzagd/Pack_40_Skin_By_Ngh%25C4%25A9aLq_TV_%255B_Android_-_IOS_%255D.zip/file"
   }
 
 };
