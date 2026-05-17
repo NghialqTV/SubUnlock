74df8b13890c2c98dedb29180091c7ea3721c886
Nghĩalq TV <nguyentrungnghia20012004@gmail.com>
2026-05-17 16:53:23 +0700

    Update data.js

--- data.js
+++ data.js
@@ -46,6 +46,18 @@ const pages = {
     unlock:
     "https://funlink.io/Puev89R"
 
-  }
+  },
+  pack40skin1705: {
 
+    sub:
+    "https://youtube.com/@nghiaxtv",
+
+    like:
+    "https://youtu.be/8e_VkmBs5Yk?si=C_xDgPkr9-7XoVu3",
+
+    tele:
+    "https://t.me/+gI22PHmUi5xhMDA9",
+
+    unlock:
+    "https://www.mediafire.com/file/6flzvt1eklfzagd/Pack_40_Skin_By_Ngh%25C4%25A9aLq_TV_%255B_Android_-_IOS_%255D.zip/file"
 };
