da0bcdf0021238ffc0d29840bdd0b4f73d6ed58f
Nghĩalq TV <nguyentrungnghia20012004@gmail.com>
2026-05-19 15:45:30 +0700

    Update unlock links in data.js

--- data.js
+++ data.js
@@ -39,14 +39,14 @@ const pages = {
     sub: "https://youtube.com/@nghiaxtv",
     like: "https://youtu.be/8e_VkmBs5Yk?si=C_xDgPkr9-7XoVu3",
     tele: "https://t.me/+gI22PHmUi5xhMDA9",
-    unlock: "https://ipas.cc/82k69h"
+    unlock: "https://linkx.me/ZyggKmr"
   },
 
   modmenuios: {
     sub: "https://youtube.com/@nghiaxtv",
     like: "https://youtu.be/8e_VkmBs5Yk?si=C_xDgPkr9-7XoVu3",
     tele: "https://t.me/+gI22PHmUi5xhMDA9",
-    unlock: "https://ipas.cc/anoq8i"
+    unlock: "https://linkx.me/VDXR"
   },
 
   keymenuios: {
