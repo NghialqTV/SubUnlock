f8c8adfdbd4c161cda34e2441b2060694f2eba40
Nghĩalq TV <nguyentrungnghia20012004@gmail.com>
2026-05-24 15:33:33 +0700

    Update TikTok ad URLs in app.js

--- app.js
+++ app.js
@@ -135,7 +135,7 @@ function verifyTasks(){
   btn.disabled = true;
   
   // Mở quảng cáo TikTok 1
-  window.open("https://vt.tiktok.com/ZS9YhEgqSYAbH-PqBKZ/", "_blank");
+  window.open("https://vt.tiktok.com/ZS9Y4cmftpDPW-CaR5R/", "_blank");
 
   let time = 5;
   btn.innerHTML = "Đang mở khóa 5s";
@@ -159,7 +159,7 @@ function openUnlock(){
   openBtn.disabled = true;
 
   // Mở quảng cáo TikTok 2
-  window.open("https://vt.tiktok.com/ZS9YhE74P5dhh-m0DJY/", "_blank");
+  window.open("https://vt.tiktok.com/ZS9Y4ccPQVuhv-fGuWm/", "_blank");
 
   // Xóa lịch sử nhiệm vụ cũ để lần sau bấm link khác vẫn làm lại được
   localStorage.removeItem(id + "_1");
