dc3e37df4df8171586df93dd1d6f8cbeb41ecac1
Nghĩalq TV <nguyentrungnghia20012004@gmail.com>
2026-05-27 17:46:09 +0700

    Update TikTok ad links in app.js

--- app.js
+++ app.js
@@ -135,7 +135,7 @@ function verifyTasks(){
   btn.disabled = true;
   
   // Mở quảng cáo TikTok 1
-  window.open("https://vt.tiktok.com/ZS9YW31VFNtbH-5ynPF/", "_blank");
+  window.open("https://vt.tiktok.com/ZS9YTkcjtPWHM-4C9Kc/", "_blank");
 
   let time = 5;
   btn.innerHTML = "Đang mở khóa 5s";
@@ -159,7 +159,7 @@ function openUnlock(){
   openBtn.disabled = true;
 
   // Mở quảng cáo TikTok 2
-  window.open("https://vt.tiktok.com/ZS9YW3J29NKUX-dAl7B/", "_blank");
+  window.open("https://vt.tiktok.com/ZS9YTkVrDMXtJ-oYmjP/", "_blank");
 
   // Xóa lịch sử nhiệm vụ cũ để lần sau bấm link khác vẫn làm lại được
   localStorage.removeItem(id + "_1");
