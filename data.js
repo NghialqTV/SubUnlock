c7149435401b978a3050d6ab6f4ca92d60c7bc58
Nghĩalq TV <nguyentrungnghia20012004@gmail.com>
2026-06-03 23:07:48 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,120 +1,79 @@
 const pages = {
   
-  "85skin2005": {
-    sub: "https://youtube.com/@nghiaxtv",
-    like: "https://youtu.be/4p4ymxUvD6Q?si=A65CN-syZQ95eI0D",
-    tele: "https://t.me/+gI22PHmUi5xhMDA9",
-    unlock: "https://nghialqtv.github.io/mod/file-le.htmlate.zip/file"
-  },
-
   filele: {
-    sub: "https://youtube.com/@nghiaxtv",
+    sub: "https://www.youtube.com/@CyberMod_04",
     like: "https://youtu.be/qad5-ztp8QA?si=SO7YZzXIwJ03fOEj",
     tele: "https://t.me/+gI22PHmUi5xhMDA9",
     unlock: "https://nghialqtv.github.io/mod/file-le.html"
   },
   
   camxa: {
-    sub: "https://youtube.com/@nghiaxtv",
+    sub: "https://www.youtube.com/@CyberMod_04",
     like: "https://youtu.be/qad5-ztp8QA?si=SO7YZzXIwJ03fOEj",
     tele: "https://t.me/+gI22PHmUi5xhMDA9",
     unlock: "https://www.mediafire.com/file/csnsh3fwcuf9sv8/Cam_Xa_S2_%255B_C%25E1%25BA%25ADp_Nh%25E1%25BA%25ADt_21_Th%25C3%25A1ng_5%255D.zip/file"
   },
   
   resources: {
-    sub: "https://youtube.com/@nghiaxtv",
+    sub: "https://www.youtube.com/@CyberMod_04",
     like: "https://youtu.be/qad5-ztp8QA?si=SO7YZzXIwJ03fOEj",
     tele: "https://t.me/+gI22PHmUi5xhMDA9",
     unlock: "https://www.mediafire.com/file/6yhybry0ns6id2d/Resources_C%25E1%25BA%25ADp_Nh%25C3%25A2t_30_Th%25C3%25A1ng_5.zip/file"
   },
   
   keyandroid: {
-    sub: "https://youtube.com/@nghiaxtv",
+    sub: "https://www.youtube.com/@CyberMod_04",
     like: "https://youtu.be/QhV7ZN5uEPM?si=hSnxddRY17XEyEii",
     tele: "https://t.me/+gI22PHmUi5xhMDA9",
     unlock: "https://duymmo.io.vn/ctvkey/3fun.php"
   },
   
   modmenuandroid64: {
-    sub: "https://youtube.com/@nghiaxtv",
+    sub: "https://www.youtube.com/@CyberMod_04",
     like: "https://youtu.be/QhV7ZN5uEPM?si=4aCO2I1TKlIQorpD",
     tele: "https://t.me/+gI22PHmUi5xhMDA9",
     unlock: "https://www.mediafire.com/file/ck8cfrapf1ptqj2/Menu_Li%25C3%25AAn_Qu%25C3%25A2n_ModSkin_.apk/file"
   },
   
   modmenuandroid32: {
-    sub: "https://youtube.com/@nghiaxtv",
+    sub: "https://www.youtube.com/@CyberMod_04",
     like: "https://youtu.be/QhV7ZN5uEPM?si=4aCO2I1TKlIQorpD",
     tele: "https://t.me/+gI22PHmUi5xhMDA9",
     unlock: "https://www.mediafire.com/file/j21h804l6ojsn44/Menu_Mod_Skin_32_Bit_Fix.apk/file"
   },
   
   hackmenuandroid64: {
-    sub: "https://youtube.com/@nghiaxtv",
+    sub: "https://www.youtube.com/@CyberMod_04",
     like: "https://youtu.be/MZLKU7WZbRA?si=dOVVzrNKDbmWbHJz",
     tele: "https://t.me/+gI22PHmUi5xhMDA9",
     unlock: "https://www.mediafire.com/file/l54f3cq8798eo6d/Menu_Hack_Map_%255B_64_Bit_%255D.apk/file"
   },
   
   ipaios: {
-    sub: "https://youtube.com/@nghiaxtv",
+    sub: "https://www.youtube.com/@CyberMod_04",
     like: "https://youtu.be/QxU_WqDb11E?si=62Fn0AM8CzZN2qn2",
     tele: "https://t.me/+gI22PHmUi5xhMDA9",
     unlock: "https://www.mediafire.com/file/l3cg6df68x7rsum/AovMod_1.62.11625414.ipa/file"
   },
   
   hackmenuios: {
-    sub: "https://youtube.com/@nghiaxtv",
+    sub: "https://www.youtube.com/@CyberMod_04",
     like: "https://youtu.be/f4OB08TciwY?si=jsoYiRMvHKnzOloJ",
     tele: "https://t.me/+gI22PHmUi5xhMDA9",
     unlock: "https://ipas.cc/l2fg9k"
   },
 
   modmenuios: {
-    sub: "https://youtube.com/@nghiaxtv",
+    sub: "https://www.youtube.com/@CyberMod_04",
     like: "https://youtu.be/f4OB08TciwY?si=jsoYiRMvHKnzOloJ",
     tele: "https://t.me/+gI22PHmUi5xhMDA9",
     unlock: "https://ipas.cc/3eo9i6"
   },
 
   keymenuios: {
-    sub: "https://youtube.com/@nghiaxtv",
+    sub: "https://www.youtube.com/@CyberMod_04",
     like: "https://youtu.be/f4OB08TciwY?si=jsoYiRMvHKnzOloJ",
     tele: "https://t.me/+gI22PHmUi5xhMDA9",
     unlock: "https://funlink.io/Puev89R"
-  },
-
-  pack40skin1705: {
-    sub: "https://youtube.com/@nghiaxtv",
-    like: "https://youtu.be/hcb2ZsVcQ1E?si=dcW7h64IvZre1C1N",
-    tele: "https://t.me/+gI22PHmUi5xhMDA9",
-    unlock: "https://www.mediafire.com/file/6flzvt1eklfzagd/Pack_40_Skin_By_Ngh%25C4%25A9aLq_TV_%255B_Android_-_IOS_%255D.zip/file"
-  },
-
-  pack60skin2405: {
-    sub: "https://youtube.com/@nghiaxtv",
-    like: "https://youtu.be/r5oaOeneRz4?si=gKhbSzrG5FRUZJza",
-    tele: "https://t.me/+gI22PHmUi5xhMDA9",
-    unlock: "https://www.mediafire.com/file/m4ac0mkbeojusn3/Mod_Pack_60_Skin_Li%25C3%25AAn_Qu%25C3%25A2n_M%25E1%25BB%259Bi_Nh%25E1%25BA%25A5t_By_NghialqTV.zip/file"
-  },
-  pack120skin3005: {
-    sub: "https://youtube.com/@nghiaxtv",
-    like: "https://youtu.be/-KChHODv5yM?si=woDJQX209jtky9gV",
-    tele: "https://t.me/+gI22PHmUi5xhMDA9",
-    unlock: "https://www.mediafire.com/file/qo94y5ub6wcdx9s/Pack_121_Skin_Android_-_IOS.zip/file"
-  },
-
-  pack56skin2205: {
-    sub: "https://youtube.com/@nghiaxtv",
-    like: "https://youtu.be/UcnKpaGXFIM?si=tzV7bl4IOSrj2254",
-    tele: "https://t.me/+gI22PHmUi5xhMDA9",
-    unlock: "https://www.mediafire.com/file/06clfhfpsl1d0ry/Pack_56_Skin_Android_-_IOS_Sau_C%25E1%25BA%25ADp_Nh%25E1%25BA%25ADt_22_Th%25C3%25A1ng_5.zip/file"
-  },
-  pack41skin2805: {
-    sub: "https://youtube.com/@nghiaxtv",
-    like: "https://youtu.be/928kxGy229A?si=v1yOdqcJZtr_lCJD",
-    tele: "https://t.me/+gI22PHmUi5xhMDA9",
-    unlock: "https://www.mediafire.com/file/utd48wxb9bjoz4d/Pack_41_Skin_Android_-_IOS.zip/file"
   }
-
 };
