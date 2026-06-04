6c5c668ae7d797103151899f9c296bd62f6c6a87
Nghĩalq TV <nguyentrungnghia20012004@gmail.com>
2026-06-04 08:16:21 +0700

    Update data.js

--- data.js
+++ data.js
@@ -2,35 +2,35 @@ const pages = {
   
   filele: {
     sub: "https://www.youtube.com/@CyberMod_04",
-    like: "https://youtu.be/qad5-ztp8QA?si=SO7YZzXIwJ03fOEj",
+    like: "https://youtu.be/oMtOiTVCNVw?si=zRmRlMtJPSzc2P2F",
     tele: "https://t.me/+gI22PHmUi5xhMDA9",
     unlock: "https://nghialqtv.github.io/mod/file-le.html"
   },
   
   camxa: {
     sub: "https://www.youtube.com/@CyberMod_04",
-    like: "https://youtu.be/qad5-ztp8QA?si=SO7YZzXIwJ03fOEj",
+    like: "https://youtu.be/oMtOiTVCNVw?si=zRmRlMtJPSzc2P2F",
     tele: "https://t.me/+gI22PHmUi5xhMDA9",
     unlock: "https://www.mediafire.com/file/csnsh3fwcuf9sv8/Cam_Xa_S2_%255B_C%25E1%25BA%25ADp_Nh%25E1%25BA%25ADt_21_Th%25C3%25A1ng_5%255D.zip/file"
   },
   
   resources: {
     sub: "https://www.youtube.com/@CyberMod_04",
-    like: "https://youtu.be/qad5-ztp8QA?si=SO7YZzXIwJ03fOEj",
+    like: "https://youtu.be/oMtOiTVCNVw?si=zRmRlMtJPSzc2P2F",
     tele: "https://t.me/+gI22PHmUi5xhMDA9",
     unlock: "https://www.mediafire.com/file/6yhybry0ns6id2d/Resources_C%25E1%25BA%25ADp_Nh%25C3%25A2t_30_Th%25C3%25A1ng_5.zip/file"
   },
   
   keyandroid: {
     sub: "https://www.youtube.com/@CyberMod_04",
-    like: "https://youtu.be/QhV7ZN5uEPM?si=hSnxddRY17XEyEii",
+    like: "https://youtu.be/oMtOiTVCNVw?si=zRmRlMtJPSzc2P2F",
     tele: "https://t.me/+gI22PHmUi5xhMDA9",
     unlock: "https://duymmo.io.vn/ctvkey/3fun.php"
   },
   
   modmenuandroid64: {
     sub: "https://www.youtube.com/@CyberMod_04",
-    like: "https://youtu.be/QhV7ZN5uEPM?si=4aCO2I1TKlIQorpD",
+    like: "https://youtu.be/oMtOiTVCNVw?si=zRmRlMtJPSzc2P2F",
     tele: "https://t.me/+gI22PHmUi5xhMDA9",
     unlock: "https://www.mediafire.com/file/ck8cfrapf1ptqj2/Menu_Li%25C3%25AAn_Qu%25C3%25A2n_ModSkin_.apk/file"
   },
@@ -72,7 +72,7 @@ const pages = {
 
   keymenuios: {
     sub: "https://www.youtube.com/@CyberMod_04",
-    like: "https://youtu.be/f4OB08TciwY?si=jsoYiRMvHKnzOloJ",
+    like: "https://youtu.be/oMtOiTVCNVw?si=zRmRlMtJPSzc2P2F",
     tele: "https://t.me/+gI22PHmUi5xhMDA9",
     unlock: "https://funlink.io/Puev89R"
   }
