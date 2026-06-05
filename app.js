1d99abdb7e8d740115173c33d93881b544f8b3a8
Nghĩalq TV <nguyentrungnghia20012004@gmail.com>
2026-06-05 12:18:15 +0700

    Update app.js

--- app.js
+++ app.js
@@ -168,17 +168,11 @@ function openUnlock(){
     window.location.href = data.unlock;
   }, 5000);
 }
-window.addEventListener("load", () => {
-    setTimeout(() => {
-        const loader = document.getElementById("loader");
-        if(loader){
-            loader.style.opacity = "0";
-            loader.style.transition = "0.5s";
-            setTimeout(() => {
-                loader.remove();
-            }, 500);
-        }
-    }, 1200);
-});
+setTimeout(() => {
+    const loader = document.getElementById("loader");
+    if (loader) {
+        loader.style.display = "none";
+    }
+}, 1500);
 /* START */
 updateProgress();
