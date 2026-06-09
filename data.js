729ac75fc88f75135f9c927eff09ffd5588dc05b
Nghĩalq TV <nguyentrungnghia20012004@gmail.com>
2026-06-09 12:11:40 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,78 +1,74 @@
 const pages = {
 
+  Fps: {
+    sub: "https://youtube.com/@cybertv_v2?si=wTDp5vhgQBlGlq-J",
+    like: "https://youtu.be/OanRin_99vQ?si=FWFIGxX9Fflyc59a",
+    tele: "https://t.me/+gI22PHmUi5xhMDA9",
+    unlock: "https://www.mediafire.com/file/4gldcj37uk9ab2w/Mod_120_FPS_Cao_Li%25C3%25AAn_Qu%25C3%25A2n_Android.zip/file"
+  },
   filele: {
     sub: "https://youtube.com/@cybertv_v2?si=wTDp5vhgQBlGlq-J",
-    like: "https://youtube.com/@cybertv_v2?si=wTDp5vhgQBlGlq-J",
+    like: "https://youtu.be/6T5qfJAGP3c?si=P3KlCv9nNPgaO8CM",
     tele: "https://t.me/+gI22PHmUi5xhMDA9",
     unlock: "https://nghialqtv.github.io/mod/file-le.html"
   },
-  
   camxa: {
     sub: "https://youtube.com/@cybertv_v2?si=wTDp5vhgQBlGlq-J",
-    like: "https://youtu.be/oMtOiTVCNVw?si=zRmRlMtJPSzc2P2F",
+    like: "https://youtu.be/94QS7vaI4I8?si=ruA9SQi-bb841zgO",
     tele: "https://t.me/+gI22PHmUi5xhMDA9",
-    unlock: "https://www.mediafire.com/file/csnsh3fwcuf9sv8/Cam_Xa_S2_%255B_C%25E1%25BA%25ADp_Nh%25E1%25BA%25ADt_21_Th%25C3%25A1ng_5%255D.zip/file"
+    unlock: "https://www.mediafire.com/file/cj8rf3k2dnmt99z/Mod_Camxa_Li%25C3%25AAn_Qu%25C3%25A2n_Android_-_IOS.zip/file"
   },
-  
   resources: {
     sub: "https://youtube.com/@cybertv_v2?si=wTDp5vhgQBlGlq-J",
-    like: "https://youtu.be/oMtOiTVCNVw?si=zRmRlMtJPSzc2P2F",
+    like: "https://youtu.be/6T5qfJAGP3c?si=i3QjRuq5TQ90Y3Ui",
     tele: "https://t.me/+gI22PHmUi5xhMDA9",
     unlock: "https://www.mediafire.com/file/fmg9hcixqsb3cwh/Resources_C%25E1%25BA%25ADp_Nh%25E1%25BA%25ADt_4_th%25C3%25A1ng_6.zip/file"
   },
-  
   keyandroid: {
     sub: "https://youtube.com/@cybertv_v2?si=wTDp5vhgQBlGlq-J",
-    like: "https://youtu.be/eMhfdpLHPq0?si=xttj9CEgEyw7rNYk",
+    like: "https://youtu.be/u8Ua7v_Z5rg?si=0rc_EDvDWYAeYulr",
     tele: "https://t.me/+gI22PHmUi5xhMDA9",
     unlock: "https://duymmo.io.vn/ctvkey/3fun.php"
   },
-  
   modmenuandroid64: {
     sub: "https://youtube.com/@cybertv_v2?si=wTDp5vhgQBlGlq-J",
-    like: "https://youtu.be/eMhfdpLHPq0?si=xttj9CEgEyw7rNYk",
+    like: "https://youtu.be/u8Ua7v_Z5rg?si=0rc_EDvDWYAeYulr",
     tele: "https://t.me/+gI22PHmUi5xhMDA9",
     unlock: "https://www.mediafire.com/file/ck8cfrapf1ptqj2/Menu_Li%25C3%25AAn_Qu%25C3%25A2n_ModSkin_.apk/file"
   },
-  
   modmenuandroid32: {
     sub: "https://youtube.com/@cybertv_v2?si=wTDp5vhgQBlGlq-J",
-    like: "https://youtu.be/eMhfdpLHPq0?si=xttj9CEgEyw7rNYk",
+    like: "https://youtu.be/u8Ua7v_Z5rg?si=0rc_EDvDWYAeYulr",
     tele: "https://t.me/+gI22PHmUi5xhMDA9",
     unlock: "https://www.mediafire.com/file/j21h804l6ojsn44/Menu_Mod_Skin_32_Bit_Fix.apk/file"
   },
-  
   hackmenuandroid64: {
     sub: "https://youtube.com/@cybertv_v2?si=wTDp5vhgQBlGlq-J",
-    like: "https://youtu.be/eMhfdpLHPq0?si=xttj9CEgEyw7rNYk",
+    like: "https://youtu.be/u8Ua7v_Z5rg?si=0rc_EDvDWYAeYulr",
     tele: "https://t.me/+gI22PHmUi5xhMDA9",
     unlock: "https://www.mediafire.com/file/l54f3cq8798eo6d/Menu_Hack_Map_%255B_64_Bit_%255D.apk/file"
   },
-  
   ipaios: {
     sub: "https://youtube.com/@cybertv_v2?si=wTDp5vhgQBlGlq-J",
     like: "https://youtu.be/QxU_WqDb11E?si=62Fn0AM8CzZN2qn2",
     tele: "https://t.me/+gI22PHmUi5xhMDA9",
     unlock: "https://www.mediafire.com/file/l3cg6df68x7rsum/AovMod_1.62.11625414.ipa/file"
   },
-  
   hackmenuios: {
     sub: "https://youtube.com/@cybertv_v2?si=wTDp5vhgQBlGlq-J",
     like: "https://youtu.be/f4OB08TciwY?si=jsoYiRMvHKnzOloJ",
     tele: "https://t.me/+gI22PHmUi5xhMDA9",
     unlock: "https://ipas.cc/byem9u"
   },
-
   modmenuios: {
     sub: "https://youtube.com/@cybertv_v2?si=wTDp5vhgQBlGlq-J",
-    like: "https://youtu.be/f4OB08TciwY?si=jsoYiRMvHKnzOloJ",
+    like: "https://youtu.be/zfOV13NgRLI?si=YBnSeJkNJZj3Ae_h",
     tele: "https://t.me/+gI22PHmUi5xhMDA9",
     unlock: "https://ipas.cc/pyev1a"
   },
-
   keymenuios: {
     sub: "https://youtube.com/@cybertv_v2?si=wTDp5vhgQBlGlq-J",
-    like: "https://youtu.be/oMtOiTVCNVw?si=zRmRlMtJPSzc2P2F",
+    like: "https://youtu.be/zfOV13NgRLI?si=YBnSeJkNJZj3Ae_h",
     tele: "https://t.me/+gI22PHmUi5xhMDA9",
     unlock: "https://funlink.io/PwQB1jV"
   },
