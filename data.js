e481855fd819d922d01cd9cf47057aa0f2a49502
Nghĩalq TV <nguyentrungnghia20012004@gmail.com>
2026-06-09 22:58:59 +0700

    Update data.js

--- data.js
+++ data.js
@@ -84,6 +84,12 @@ const pages = {
     tele: "https://t.me/+gI22PHmUi5xhMDA9",
     unlock: "https://linkx.me/jUdf8f0"
   },
+  mod65skin1006: {
+    sub: "https://youtube.com/@cybertv_v2?si=wTDp5vhgQBlGlq-J",
+    like: "https://youtu.be/_Jo7z-mUlwU?si=Q59dzeu1Onx2ZZmH",
+    tele: "https://t.me/+gI22PHmUi5xhMDA9",
+    unlock: "https://gofile.io/d/jnHUzb"
+  },
   subriece: {
     sub: "https://youtube.com/@cybertv_v2?si=6eBpBS8-aVn6E0VF",
     like: "https://youtu.be/ZKuslIWq1gM?si=Inu9oENn-c6GPGIJ",
