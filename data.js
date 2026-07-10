bea4d11bf2f8e962bcfa71d99618c0c651a9e6d2
Nghĩalq TV <nguyentrungnghia20012004@gmail.com>
2026-07-10 20:05:45 +0700

    Update data.js

--- data.js
+++ data.js
@@ -49,7 +49,7 @@ const pages = {
   },
   modmenuios: {
     like: "https://youtu.be/zrCz3IJkNmM?si=eCRwcIFPEIzrNkxO",
-    unlock: "https://linkx.me/o7jJK"
+    unlock: "https://linkx.me/pAQHHZ2"
   },
   keymenuios: {
     like: "https://youtu.be/zrCz3IJkNmM?si=eCRwcIFPEIzrNkxO",
