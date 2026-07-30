66ad998e0b9cd610c5921894dea3bb12c6013df4
Nghĩalq TV <nguyentrungnghia20012004@gmail.com>
2026-07-30 19:34:55 +0700

    Update data.js

--- data.js
+++ data.js
@@ -25,7 +25,7 @@ const pages = {
   },
   resources: {
     like: "https://youtu.be/rKJSqaRlKgs?si=kiUN92ZkRwYPwgqT",
-    unlock: "https://www.mediafire.com/file/is7cxfo0d77iagf/Resources_C%25E1%25BA%25ADp_Nh%25E1%25BA%25ADt_30Th%25C3%25A1ng7.zip/file"
+    unlock: "https://www.mediafire.com/file/dvq2p1ggvesgh7k/Resources_Fix_Reset_Mod_C%25E1%25BA%25ADp_Nh%25E1%25BA%25ADt_30_Th%25C3%25A1ng_7_By_Ngh%25C4%25A9alq_TV.zip/file"
   },
   keyandroid: {
     like: "https://youtu.be/iGYxtDpFP70?si=c6kzC-qD6C-xjIx9",
