a9e26eca6a30eaaf886a5e5059d09ce97b203c59
Nghĩalq TV <nguyentrungnghia20012004@gmail.com>
2026-08-09 17:52:48 +0700

    Update data.js

--- data.js
+++ data.js
@@ -120,7 +120,7 @@ const pages = {
     unlock: "https://www.mediafire.com/file/c228ivjvcdu4x0e/Mod_Full_500_Skin_Max_In_5.zip/file"
   },
   mod123skin0908: {
-    like: "https://youtu.be/6rFLGFTJquM?si=OS9dFSVSm8dyVbjz",
+    like: "https://youtu.be/gLRC-w_Cwzg?si=_uzKh0_sgaUSXXI4",
     unlock: "https://www.mediafire.com/file/voikesbl8avbmc7/Mod_Full_123_Skin_C%25C3%25B3_N%25C3%25BAt_B%25E1%25BA%25A5m_By_Ngh%25C4%25A9aLQ_TV.zip/file"
   },
 };
