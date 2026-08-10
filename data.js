35dac7852132ba64dfd6b4e82195bf0772ff49a7
Nghĩalq TV <nguyentrungnghia20012004@gmail.com>
2026-08-11 00:07:30 +0700

    Update data.js

--- data.js
+++ data.js
@@ -50,7 +50,7 @@ const pages = {
   },
   hackmenuios: {
     like: "https://youtu.be/zrCz3IJkNmM?si=eCRwcIFPEIzrNkxO",
-    unlock: "https://linkx.me/nwPwOyQ"
+    unlock: "https://linkx.me/3l0VjF"
   },
   modmenuios: {
     like: "https://youtu.be/zrCz3IJkNmM?si=eCRwcIFPEIzrNkxO",
