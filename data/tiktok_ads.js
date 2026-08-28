5727c588e4300377c83eba02c86d0c4f7c52e1b9
Nghĩalq TV <nguyentrungnghia20012004@gmail.com>
2026-08-28 22:45:15 +0700

    Add files via upload

---
+++ data/tiktok_ads.js
@@ -0,0 +1,11 @@
+/*
+ * DỮ LIỆU 5 QUẢNG CÁO TIKTOK
+ * Chỉ cần thay các link bên dưới khi muốn đổi quảng cáo.
+ */
+window.TIKTOK_AD_LINKS = [
+  'https://vt.tiktok.com/ZS9BffhjJvMjg-7nUQf/',
+  'https://vt.tiktok.com/ZS9BffAAyWaea-xg3is/',
+  'https://vt.tiktok.com/ZS9Bfyw6bo54H-GaRRP/',
+  'https://vt.tiktok.com/ZS9Bff6vBGnte-RvTZb/',
+  'https://vt.tiktok.com/ZS9BffP9MM57L-1mFTj/'
+];
