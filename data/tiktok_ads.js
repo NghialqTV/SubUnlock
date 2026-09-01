182f78d694edfb88d4f8babfb017068772b8857b
Nghĩalq TV <nguyentrungnghia20012004@gmail.com>
2026-09-02 00:13:12 +0700

    Update tiktok_ads.js

--- data/tiktok_ads.js
+++ data/tiktok_ads.js
@@ -3,9 +3,9 @@
  * Chỉ cần thay các link bên dưới khi muốn đổi quảng cáo.
  */
 window.TIKTOK_AD_LINKS = [
-  'https://vt.tiktok.com/ZS9BffhjJvMjg-7nUQf/',
-  'https://vt.tiktok.com/ZS9BffAAyWaea-xg3is/',
-  'https://vt.tiktok.com/ZS9Bfyw6bo54H-GaRRP/',
-  'https://vt.tiktok.com/ZS9Bff6vBGnte-RvTZb/',
-  'https://vt.tiktok.com/ZS9BffP9MM57L-1mFTj/'
+  'https://vt.tiktok.com/ZS9Bsh9ds8Xdg-vUBHM/',
+  'https://vt.tiktok.com/ZS9Bsh5GP5S5y-JYQI5/',
+  'https://vt.tiktok.com/ZS9BshPe5hJQK-6cS4f/',
+  'https://vt.tiktok.com/ZS9BshDF6DVnM-3otFN/',
+  'https://vt.tiktok.com/ZS9Bsh2TY61qE-OkV7l/'
 ];
